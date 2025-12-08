4209b739bf054a1c479eadebd3ddddbca07769af
Financypants <isaacjhuntsman@gmail.com>
2025-12-07 19:31:31 -0700

    Initial Commit

---
+++ joi/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  joi
+//
+//  Created by Isaac Huntsman on 12/7/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
